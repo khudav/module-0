7c7caf0757cc50d0be8f31350092bd501d5f3c23
khudav <84847707+khudav@users.noreply.github.com>
2021-05-26 23:47:58 +0300

    Add files via upload

--- cross-zero.py
+++ cross-zero.py
@@ -1,59 +1,74 @@
-print('*' * 3, ' Игра в "крестики-нолики" для двух игроков ', '*' * 3)
+print('*' * 4, ' Игра в "крестики-нолики" для двух игроков ', '*' * 4)
+print('В поле ввода необходимо указывать номер клетки (1-9).')
+board = list(range(1, 10)) # задаем список номеров клеток
 
-board = list(range(1, 10))
 
 def draw_board(board):
-   print("-" * 13)
-   for i in range(3):
-      print("|", board[0+i*3], "|", board[1+i*3], "|", board[2+i*3], "|")
-      print("-" * 13)
+    """Функция рисует игровое поле в привычном для человека формате."""
+    print(" " * 13)
+    for i in range(3):
+        print(" ", board[0+i*3], " ", board[1+i*3], " ", board[2+i*3], " ")
+        print(" " * 13)
+
 
 def take_input(player_token):
-   valid = False
-   while not valid:
-      player_answer = input("Куда поставим " + player_token+"? ")
-      try:
-         player_answer = int(player_answer)
-      except:
-         print("Некорректный ввод. Вы уверены, что ввели число?")
-         continue
-      if player_answer >= 1 and player_answer <= 9:
-         if(str(board[player_answer-1]) not in "XO"):
-            board[player_answer-1] = player_token
-            valid = True
-         else:
-            print("Эта клетка уже занята!")
-      else:
-        print("Некорректный ввод. Введите число от 1 до 9.")
+    """Функция принимает ввод пользователя. Проверяет корректность ввода."""
+    valid = False
+    while not valid:
+        player_answer = input("Куда поставим " + player_token+"? ")
+        try:
+            player_answer = int(player_answer)
+        except:
+            print("Некорректный ввод. Вы уверены, что ввели число?")
+            continue
+
+        if player_answer >= 1 and player_answer <= 9:
+            if(str(board[player_answer-1]) not in "XO"):
+                board[player_answer-1] = player_token
+                valid = True
+            else:
+                print("Эта клетка уже занята!")
+        else:
+            print("Некорректный ввод. Введите число от 1 до 9.")
+
 
 def check_win(board):
-   win_coord = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
-   for each in win_coord:
-       if board[each[0]] == board[each[1]] == board[each[2]]:
-          return board[each[0]]
-   return False
+    """Функция проверки игрового поля, проверяет, выиграл ли игрок."""
+    win_coord = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
+    for each in win_coord:
+        if board[each[0]] == board[each[1]] == board[each[2]]:
+            return board[each[0]]
+
+    return False
+
 
 def main(board):
+    """Функция запускает и управляет игровым процессом."""
     counter = 0
     win = False
     while not win:
         draw_board(board)
         if counter % 2 == 0:
-           take_input("X")
+            take_input("X")
         else:
-           take_input("O")
+            take_input("O")
+
         counter += 1
+
         if counter > 4:
-           tmp = check_win(board)
-           if tmp:
-              print(tmp, "выиграл!")
-              win = True
-              break
+            tmp = check_win(board)
+            if tmp:
+                print(tmp, "выиграл!")
+                win = True
+                break
+
         if counter == 9:
             print("Ничья!")
             break
+
     draw_board(board)
+
 
 main(board)
 
-input('Нажмите Enter для завершения игры.')
+input('Нажмите Enter для выхода из игры.')
